000d4e0d2ba277c9623024d7da4e296d0b58a4a3
Nil3138 <167197477+Nil3138@users.noreply.github.com>
2024-04-17 08:41:47 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,11 @@
+listasNomes = []
+
+listasNomes.append("Edson")
+listasNomes.append("Tamires")
+listasNomes.append("Sofia")
+
+print(listasNomes[1])
+
+for nome in listasNomes:
+    print(nome)
+    
